4dcd56af419b505665800dc9ca1dbc2eb59f31e6
huy nguyen <huynt57@gmail.com>
2017-10-31 17:35:45 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,6 +48,8 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.backend' => \App\Http\Middleware\AdminAuthenticate::class,
+        'auth.frontend' => \App\Http\Middleware\FrontendAuthenticate::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
---
+++ app/Http/Middleware/FrontendAuthenticate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class FrontendAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if (auth('frontend')->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest('dang-nhap');
+            }
+        }
+
+        return $next($request);
+    }
+}
