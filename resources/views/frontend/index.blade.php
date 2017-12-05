ea341a393e1a914265a24d5e8ef5cb627d562cfd
huy nguyen <huynt57@gmail.com>
2017-12-05 17:07:54 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,40 +1,41 @@
 @extends('frontend')
 
 @section('content')
+    @php $slide = \App\Models\Slide::all(); @endphp
     <div class="ht-ht-after-lg-header" style=" background: #fff">
         <div class="home-search-wrapper">
             <div class="tab-content">
                 <div role="tabpanel" class="tab-pane fade in active ht-bgcover" id="demoSearchTab01"
-                     style="background-image: url('stock/stock-01.jpg')">
+                     style="background-image: url('{{ $slide[0]->path }}')">
                     <div class="container">
                         <div class="text-center">
-                            <h1 class="tab-heading-title">Đường về mái ấm
+                            <h1 class="tab-heading-title">{{ $slide[0]->name }}
 
                             </h1>
                         </div>
                     </div>
                 </div>
                 <div role="tabpanel" class="tab-pane ht-bgcover fade" id="demoSearchTab02"
-                     style="background-image: url('stock/stock-02.jpg')">
+                     style="background-image: url('{{ $slide[1]->path }}')">
                     <div class="container">
                         <div class="text-center">
-                            <h1 class="tab-heading-title">Ngôi nhà bạn sẽ thuê</h1>
+                            <h1 class="tab-heading-title">{{ $slide[1]->name }}</h1>
                         </div>
                     </div>
                 </div>
                 <div role="tabpanel" class="tab-pane ht-bgcover fade" id="demoSearchTab03"
-                     style="background-image: url('stock/stock-03.jpg')">
+                     style="background-image: url('{{ $slide[2]->path }}')">
                     <div class="container">
                         <div class="text-center">
-                            <h1 class="tab-heading-title">Hãy trở thành nhà đầu tư thông thái</h1>
+                            <h1 class="tab-heading-title">{{ $slide[2]->name }}</h1>
                         </div>
                     </div>
                 </div>
                 <div role="tabpanel" class="tab-pane ht-bgcover fade" id="demoSearchTab04"
-                     style="background-image: url('stock/stock-04.jpg')">
+                     style="background-image: url('{{ $slide[3]->path }}')">
                     <div class="container">
                         <div class="text-center">
-                            <h1 class="tab-heading-title">Hiện thực hóa giấc mơ</h1>
+                            <h1 class="tab-heading-title">{{ $slide[3]->name }}</h1>
                         </div>
                     </div>
                 </div>
